7c284811858957b00153f33ffa948b167a60b377
Iarina Dafin <itd1e14@soton.ac.uk>
2020-06-14 16:49:29 +0100

    colour changes, slight refactor

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ function changeBackground () {
     $('body').css('background-color', '#2E3A4D');
     console.log("checked");
   } else {
-    $('body').css('background-color', '#CB9656');
+    $('body').css('background-color', '#F6F4F1');
     console.log("unchecked");
   }
 
